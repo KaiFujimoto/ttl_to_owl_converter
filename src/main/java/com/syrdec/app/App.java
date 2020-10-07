cf235f71700001fbae9adb552f4ff26fbdc2b640
Eric Albers <ealbers@syrdec.com>
2020-10-07 08:14:14 -0400

    Created ttl_to_owl_converter.jar, a executable jar run with java -jar ttl_to_owl_converter.jar infile.omn outfile

--- src/main/java/com/syrdec/app/App.java
+++ src/main/java/com/syrdec/app/App.java
@@ -14,37 +14,48 @@ import org.semanticweb.owlapi.formats.*;
 
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) 
     {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        String owlFileName = args[1]+".owl";
+        String rdfFileName = args[1]+".rdf";
 
-        try {
+        try 
+        {
             OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(args[0])); // will take in the first argument from the command line
             System.out.println("Number of axioms: " + ontology.getAxiomCount());
             OWLDocumentFormat inputFormat = manager.getOntologyFormat(ontology);
             
             // Export to OWL/XML
-            File outputFile = new File(args[1] + ".owl");
+            File outputFile = new File(owlFileName);
             OWLDocumentFormat outputFormat = new OWLXMLDocumentFormat();
-            if (inputFormat.isPrefixOWLDocumentFormat() && outputFormat.isPrefixOWLDocumentFormat()) {
+            
+            if (inputFormat.isPrefixOWLDocumentFormat() && outputFormat.isPrefixOWLDocumentFormat()) 
+            {
                 outputFormat.asPrefixOWLDocumentFormat().copyPrefixesFrom(inputFormat.asPrefixOWLDocumentFormat());
             }
             manager.saveOntology(ontology, outputFormat, IRI.create(outputFile.toURI()));
 
             // Export to RDF/XML
-            File outputFile2 = new File(args[1] + ".rdf");
+            File outputFile2 = new File(rdfFileName);
             RDFDocumentFormat outputFormat2 = new RDFXMLDocumentFormat();
-            if (inputFormat.isPrefixOWLDocumentFormat() && outputFormat2.isPrefixOWLDocumentFormat()) {
+            if (inputFormat.isPrefixOWLDocumentFormat() && outputFormat2.isPrefixOWLDocumentFormat()) 
+            {
                 outputFormat2.asPrefixOWLDocumentFormat().copyPrefixesFrom(inputFormat.asPrefixOWLDocumentFormat());
             }
             manager.saveOntology(ontology, outputFormat2, IRI.create(outputFile2.toURI()));
 
-        } catch (Exception e) {
-            System.out.println("Something went wrong.");
-        } finally {
-            System.out.println("The 'try catch' is finished.");
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Something went wrong. Message: "+e.getMessage());
+            System.exit(-1);
+        } 
+        finally 
+        {
+           // System.out.println("The 'try catch' is finished.");
         }
-
+        System.out.println(owlFileName+" and "+rdfFileName+" created from "+args[0]);
         System.exit(0);
     }
 }
